f6a35cfc69ad3e43821bd0387a3ce59f040a6c0f
shahdat-sajib <shahdat35-2757@diu.edu.bd>
2022-05-31 16:17:01 +0600

    cache adding

--- sw.js
+++ sw.js
@@ -1,14 +1,33 @@
+const staticCacheName = 'site-static';
+const assets = [
+    '/',
+    '/index.html',
+    '/js/app.js',
+    '/js/ui.js',
+    '/css/styles.css',
+    '/css/materialize.min.css',
+    '/img/logo.png',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+];
+
 //install service worker
 self.addEventListener('install', (evt => {
-    console.log('service worker has been installed');
+    // console.log('service worker has been installed');
+    evt.waitUntill(
+        caches.open(staticCacheName).then(cache => {
+        console.log('caching shell assets');
+        cache.addAll(assets);
+        })
+    );
+    
 }));
 
 //activate service worker
 self.addEventListener('activate', evt => {
-    console.log('Service worker has been activated');
+    // console.log('Service worker has been activated');
 });
 
 //fetch event
 self.addEventListener('fetch', evt => {
-    console.log('fetch event', evt);
+    // console.log('fetch event', evt);
 });
