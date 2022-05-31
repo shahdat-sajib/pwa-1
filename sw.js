3fc8f2a93a2443de515b1a340b4150ccc08f70e8
shahdat-sajib <shahdat35-2757@diu.edu.bd>
2022-05-31 15:25:00 +0600

    fetch event

--- sw.js
+++ sw.js
@@ -6,4 +6,9 @@ self.addEventListener('install', (evt => {
 //activate service worker
 self.addEventListener('activate', evt => {
     console.log('Service worker has been activated');
-})
+});
+
+//fetch event
+self.addEventListener('fetch', evt => {
+    console.log('fetch event', evt);
+});
