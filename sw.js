62bb80052654c06ed89cb221df584ab97d47de20
shahdat-sajib <shahdat35-2757@diu.edu.bd>
2022-05-31 23:59:47 +0600

    dynamic cache added

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const staticCacheName = 'site-static';
+const staticCacheName = 'site-static-v1';
+const dynamicCache = 'site-dynamic-v1';
 const assets = [
     '/',
     '/index.html',
@@ -41,7 +42,12 @@ self.addEventListener('fetch', evt => {
     // console.log('fetch event', evt);
     evt.respondWith(
         caches.match(evt.request).then(cacheRes => {
-            return cacheRes || fetch(evt.request);
+            return cacheRes || fetch(evt.request).then(fetchRes => {
+                return caches.open(dynamicCache).then(cache => {
+                    cache.put(evt.request.url, fetchRes.clone());
+                    return fetchRes; 
+                })
+            });
         })
     );
 });
